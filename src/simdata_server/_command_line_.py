04e462a03fa0a568e05d329608b13f076e38f35d
Thomas Rometsch <thomas.rometsch@gmail.com>
2021-10-11 18:20:51 +0200

    fix port and host args

--- src/simdata_server/_command_line_.py
+++ src/simdata_server/_command_line_.py
@@ -3,7 +3,7 @@ import argparse
 def main():
     options = parse_cli()
 
-    if options.function == "server":
+    if options.role == "server":
         from simdata_server.server import server as func
     else:
         from simdata_server.client import client as func
@@ -14,22 +14,20 @@ def parse_cli():
     
     
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--port", type=int, help="Server port")
-    parser.add_argument("--host", type=str, help="Host running the server.")
-    parser.add_argument("-v", action="store_true", help="Enable verbose output.")
 
     subparsers = parser.add_subparsers()
     
     server_parser = subparsers.add_parser("server", help="Server mode.")
     client_parser = subparsers.add_parser("client", help="Client mode.")
-    
-    server_parser.add_argument("--host", type=str, default="localhost",
-                        help="Server address")
-    server_parser.add_argument("--port", type=int, default=-1,
-                        help="Server port")
+
+    for p in [server_parser, client_parser]:
+        p.add_argument("--port", type=int, default=-1, help="Server port")
+        p.add_argument("--host", type=str, default="localhost", help="Host running the server.")
+        p.add_argument("-v", action="store_true", help="Enable verbose output.")
+
     server_parser.add_argument("--start", action="store_true")
     server_parser.add_argument("--restart", action="store_true")
+    server_parser.set_defaults(role="server")
 
 
 
@@ -48,6 +46,9 @@ def parse_cli():
     client_parser.add_argument("-k", "--kill", action="store_true",
                         help="Kill the server.")
     client_parser.add_argument("--ping", action="store_true", help="Ping the server.")
+    client_parser.set_defaults(role="client")
+    
+    
     
     options = parser.parse_args()
     return options
